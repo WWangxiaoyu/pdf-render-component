0b8c2b94a60e49ffb1e29a569ffc793523783877
王晓羽 <wangxiaoyuit@chainamobile.com>
2022-09-30 17:13:30 +0800

    20220930

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,68 +1,97 @@
 <template>
   <div class="pdf-render">
-    <h1>pdf预览</h1>
     <div style="text-align:center">
       <span>
         当前pdf总页数为: {{totalPageNum}}
       </span>
     </div>
-    <canvas id="pdfCanvas"></canvas>
+    <div :style="`margin:0 auto;width:${pdfWidth};`">
+      <canvas
+        v-for="page in totalPageNum"
+        :key="page" 
+        :id="'pdfCanvas' + page"></canvas>
+    </div>
   </div>
 </template>
 
 <script>
-import PDFJS from 'pdfjs-dist/legacy/build/pdf'
-
+const PDFJS = require("pdfjs-dist")
+PDFJS.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.5.207/pdf.worker.js"
 export default {
   name: 'pdf-render',
-  //props: ['pdfUrl'],
   data () {
     return {
-      pageNum: 1,
+      pdfSrc: "",
+      pdfDoc: null, //文档内容
+      pdfWidth: "",
+      // pageNum: 1,
       pageRendering: false,
-      pageNumPending: null,
-      pdfDoc: null,
-      totalPageNum: 0,
-      pdfUrl: 'http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf'
-    }
+      pageNumPending: null, //待处理的页码
+      totalPageNum: [],
+      scale: 1  //放大倍数
+    };
+  },
+  mounted() {
+    this.getPdfUrl();
   },
   methods: {
+    //获取本地pdf的url
+    getPdfUrl(){
+      this.pdfSrc = "/static/2022创造营学员手册.pdf"
+      this.loadFile(this.pdfSrc)
+    },
+    loadFile(url){
+      let loadingTask = PDFJS.getDocument(url);
+      loadingTask.promise.then(pdf => {
+        this.pdfDoc = pdf;
+        this.totalPageNum = pdf.numPages;
+        //$nextTick()作用：
+        this.$nextTick(()=>{
+          this.renderPage(1); //初始渲染首页
+        })
+      })
+    },
     renderPage(num) {
-      let vm = this;
-      this.pageRendering = true;
-      let canvas = document.getElementById('pdfCanvas')
-      this.pdfDoc.getPage(num).then(function(page){
-        var viewport = page.getViewport(vm.scale);
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
+      const that = this;
+      this.pdfDoc.getPage(num).then(page => {
+        let canvas = document.getElementById("pdfCanvas" + num);
+        let ctx = canvas.getContext("2d");
+        let dpr = window.devicePixelRatio || 1;
+        let bsr =
+          ctx.webkitBackingStorePixelRatio ||
+          ctx.mozBackingStorePixelRatio ||
+          ctx.msBackingStorePixelRatio ||
+          ctx.oBackingStorePixelRatio ||
+          ctx.backingStorePixelRatio ||
+          1;
+        let ratio = dpr / bsr;
+        let viewport = page.getViewport({ scale: this.scale });
+        canvas.width = viewport.width * ratio;
+        canvas.height = viewport.height * ratio;
 
-        var renderContext = {
-            canvasContext: vm.ctx,
+        canvas.style.width = viewport.width + "px";
+
+        that.pdfWidth = viewport.width + "px";
+
+        canvas.style.height = viewport.height + "px";
+
+        ctx.setTransform(ratio, 0, 0, ratio, 0, 0);
+
+
+        //将pdf页面渲染到canvas上下文中
+        let renderContext = {
+            canvasContext: ctx,
             viewport: viewport
         };
-        var renderTask = page.render(renderContext);
 
-        renderTask.promise.then(function() {
-            vm.pageRendering = false;
-            if(vm.pageNumPending !== null) {
-              // New page rendering is pending
-              vm.renderPage(vm.pageNumPending);
-              vm.pageNumPending = null;
-            }
-        });
-
+        page.render(renderContext);
+        if(this.totalPageNum>num){
+          this.renderPage(num + 1);
+        }
+        
       })
     }
   },
-  mounted() {
-    let vm = this;
-    PDFJS.getDocument(vm.pdfUrl).then(function(pdfDoc_) { //初始化pdf
-        vm.pdfDoc = pdfDoc_;
-        vm.totalPageNum = vm.pdfDoc.numPages;
-        vm.renderPage(vm.pageNum);
-    });
-
-  }
 }
 </script>
 
@@ -77,3 +106,5 @@ export default {
   }
 }
 </style>
+
+
